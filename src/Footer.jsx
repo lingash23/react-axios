ea4c82760a5df8aee146482efb3aa4d61dd02933
lingash23 <lingash2000@gmail.com>
2024-08-12 18:56:28 +0530

    all

---
+++ src/Footer.jsx
@@ -0,0 +1,12 @@
+// Footer.js
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="footer fixed-bottom">
+      <p>© 2024 Website</p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/Nav.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Nav = () => {
+  return (
+    <>
+      <nav className="navbar  p-3 fixed-top nav-main">
+        <div className="container-fluid">
+          <h3 className="navbar-brand">
+             <> </>Data Management System
+          </h3>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Nav;
